56c1f464f3e30f6a32463af9eb5d2ef3a54b9b14
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-10 21:50:55 -0700

    Rename gameClient.js to render.js

--- client/js/render.js
+++ client/js/render.js
@@ -24,38 +24,45 @@ function resizeRenderer() {
 function loadContent() {
 	PIXI.loader
 		.add("./img/ships/default.svg")
-		.load(finishedLoading);
+		.load(setup);
 }
 
-function finishedLoading() {
+function setup() {
 	ship = new PIXI.Sprite(PIXI.loader.resources["./img/ships/default.svg"].texture);
 	ship.anchor.set(0.5, 0.5);
 	ship.width = 64;
 	ship.height = 64;
 
+	setPlayerSprite(ship);
 	stage.addChild(ship);
 
 	window.requestAnimationFrame(update);
 }
 
 function update() {
-	draw();
-}
-
-function draw() {
-	centerOn(ship);
-
-	ship.rotation += 0.01;
+	updatePlayer();
 
 	renderer.render(stage);
 	window.requestAnimationFrame(update);
 }
 
+function getRenderer() {
+	return renderer;
+}
+
+function getStage() {
+	return stage;
+}
+
+function getMousePosition() {
+	return stage.toLocal(renderer.plugins.interaction.mouse.global);
+}
+
 function centerOn(sprite) {
 	stage.position.x = renderer.width / 2;
 	stage.position.y = renderer.height / 2;
-	// stage.scale.x = 2.0;
-	// stage.scale.y = 2.0;
+	stage.scale.x = 2.0;
+	stage.scale.y = 2.0;
 	stage.pivot.x = sprite.position.x;
 	stage.pivot.y = sprite.position.y;
 }
