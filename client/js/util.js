a8248b79da3f6fb6d2acbf584b0541c5edd85b95
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-15 01:03:49 -0700

    Add lerpAngle

--- client/js/util.js
+++ client/js/util.js
@@ -1,3 +1,13 @@
 function lerp(v0, v1, t) {
 	return (1 - t) * v0 + t * v1;
 }
+
+function shortAngleDist(a0,a1) {
+    var max = Math.PI*2;
+    var da = (a1 - a0) % max;
+    return 2*da % max - da;
+}
+
+function lerpAngle(a0,a1,t) {
+    return a0 + shortAngleDist(a0, a1) * t;
+}
