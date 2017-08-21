ce30e954a641932cb9e54cd6ae7c723877f25d25
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-21 00:11:47 -0700

    Add option for per-entity lerp factors

--- client/js/entity/EntityBase.js
+++ client/js/entity/EntityBase.js
@@ -5,13 +5,13 @@ class EntityBase {
 		this.sprite = null;
 		this.x = data.x || 0;
 		this.y = data.y || 0;
-		this.radius = data.radius || 16;
+		this.rotation = data.rotation || 0;
 	}
 
 	update() {
 		if (this.sprite != undefined) {
-			this.sprite.x = lerp(this.sprite.x, this.x, ENT.lerpFactorPosition);
-			this.sprite.y = lerp(this.sprite.y, this.y, ENT.lerpFactorPosition);
+			this.sprite.x = lerp(this.sprite.x, this.x, this.lerpFactorPosition || ENT.lerpFactorPosition);
+			this.sprite.y = lerp(this.sprite.y, this.y, this.lerpFactorPosition || ENT.lerpFactorPosition);
 		}
 	}
 
