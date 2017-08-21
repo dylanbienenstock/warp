2c2bd67bc2d6ad15c77c0788aa484a57a736edb7
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-20 18:38:30 -0700

    Complete EntityPhysicsDebug.js

--- entity/EntityPhysicsDebug.js
+++ entity/EntityPhysicsDebug.js
@@ -1,5 +1,20 @@
 module.exports = function(EntityBase, ENT, PHYS) {
 	return class EntityPhysicsDebug extends EntityBase {
-		// TO DO: Write this class :^)
+		constructor(data) {
+			super("PhysicsDebug", 0, 0);
+
+			this.physicsObject = data.physicsObject;
+			this.info = PHYS.getPhysicsInfo(this.physicsObject);
+		}
+
+		update() {
+			this.info = PHYS.getPhysicsInfo(this.physicsObject);
+		}
+
+		network() {
+			ENT.sendProperties(this, {
+				info: this.info
+			});
+		}
 	}
 }
