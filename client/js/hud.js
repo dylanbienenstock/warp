bef7ef336ff67893c21667df03e2b8019a74f2cf
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-21 00:21:08 -0700

    Add hud.js

---
+++ client/js/hud.js
@@ -0,0 +1,13 @@
+var radarSize = 128;
+var radarX;
+var radarY;
+var radarBorder;
+
+function setupHUD(baseContainer) {
+	radarBorder = new PIXI.Graphics();
+	baseContainer.addChild(radarBorder);
+}
+
+function drawHUD() {
+
+}
