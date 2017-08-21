6699298b0016dee4b24812d6ed5256f9b3a32475
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-21 00:11:56 -0700

    Add EntityLaser.js

---
+++ client/js/entity/EntityLaser.js
@@ -0,0 +1,26 @@
+class EntityLaser extends EntityBase {
+	constructor(data) {
+		super(data);
+
+		this.lerpFactorPosition = 0.6;
+
+		this.graphics = new PIXI.Graphics();
+		this.graphics.x = this.x;
+		this.graphics.y = this.y;
+		this.graphics.lineStyle(2, 0xFF0000, 1);
+		this.graphics.moveTo(-48, 0);
+		this.graphics.lineTo(0, 0);
+		this.graphics.rotation = this.rotation;
+
+		this.sprite = this.graphics;
+		ENT.stageContainer.addChild(this.graphics);
+	}
+
+	update() {
+		super.update();
+	}
+
+	remove() {
+		ENT.stageContainer.removeChild(this.graphics);
+	}
+}
