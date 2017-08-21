448159aef10e95da194cd4eb8b390fe5c7251b2d
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-21 00:17:03 -0700

    Add Laser to entity list

--- entity/Entity.js
+++ entity/Entity.js
@@ -1,8 +1,9 @@
 module.exports = function(io, ENT, PHYS) {
-	var Base = require("../entity/EntityBase.js")(ENT);
+	var Base = require("../entity/EntityBase.js")(ENT, PHYS);
 
 	return {
 		PhysicsDebug: require("../entity/EntityPhysicsDebug.js")(Base, ENT, PHYS),
-		Player: require("../entity/EntityPlayer.js")(Base, ENT, PHYS)
+		Player: require("../entity/EntityPlayer.js")(Base, ENT, PHYS),
+		Laser: require("../entity/EntityLaser.js")(Base, ENT, PHYS)
 	};
 }
