144066fbf660f2e51ad09fa701d566da9991f8fa
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-20 18:37:12 -0700

    Add PhysicsDebug to Entity

--- entity/Entity.js
+++ entity/Entity.js
@@ -2,6 +2,7 @@ module.exports = function(io, ENT, PHYS) {
 	var Base = require("../entity/EntityBase.js")(ENT);
 
 	return {
+		PhysicsDebug: require("../entity/EntityPhysicsDebug.js")(Base, ENT, PHYS),
 		Player: require("../entity/EntityPlayer.js")(Base, ENT, PHYS)
 	};
 }
