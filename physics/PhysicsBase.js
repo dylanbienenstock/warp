92f83566e26e4f0de523082bf7cf32871e950563
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-24 05:43:21 -0700

    Add PhysicsBase.js

---
+++ physics/PhysicsBase.js
@@ -0,0 +1,33 @@
+var PHYS;
+
+module.exports = function(__PHYS) {
+	PHYS = __PHYS;
+
+	return class PhysicsBase {
+		constructor(data) {
+			this.doNotNetwork = true;
+			this.id = -1;
+			this.x = data.x || 0;
+			this.y = data.y || 0;
+			this.localX = data.localX || 0;
+			this.localY = data.localY || 0;
+			this.thrustX = data.thrustX || 0;
+			this.thrustY = data.thrustY || 0;
+			this.velocityX = data.velocityX || 0;
+			this.velocityY = data.velocityY || 0;
+			this.children = [];
+		}
+
+		get totalVelocityX() {
+			return this.velocityX + this.thrustX;
+		}
+
+		get totalVelocityY() {
+			return this.velocityY + this.thrustY;
+		}
+
+		addChild(child) {
+			this.children.push(child);
+		}
+	}
+}
