dd2780192fd41cd5814a3e188e3518dbc47c4670
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-24 01:22:35 -0700

    Add EntityShield to entity list

--- entity/Entity.js
+++ entity/Entity.js
@@ -4,6 +4,7 @@ module.exports = function(io, ENT, PHYS) {
 	return {
 		PhysicsDebug: require("../entity/EntityPhysicsDebug.js")(Base, ENT, PHYS),
 		Player: require("../entity/EntityPlayer.js")(Base, ENT, PHYS),
+		Shield: require("../entity/EntityShield.js")(Base, ENT, PHYS),
 		Laser: require("../entity/EntityLaser.js")(Base, ENT, PHYS)
 	};
 }
