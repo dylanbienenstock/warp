c7939cdecf531b843964b5cce613e22cef425ce9
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-24 01:21:38 -0700

    Add overlay sprite

--- client/js/entity/EntityPlayer.js
+++ client/js/entity/EntityPlayer.js
@@ -2,6 +2,8 @@ class EntityPlayer extends EntityBase {
 	constructor(data) {
 		super(data);
 
+		this.triggers.hit = this.onHit.bind(this); 
+
 		this.name = data.name;
 		this.isLocalPlayer = false;
 		this.x = 0;
@@ -22,6 +24,12 @@ class EntityPlayer extends EntityBase {
 		this.sprite.width = 32;
 		this.sprite.height = 32;
 
+		this.overlaySprite = new PIXI.Sprite(PIXI.loader.resources["ship:default"].texture);
+		this.overlaySprite.anchor.set(0.678, 0.5);
+		this.overlaySprite.width = 32;
+		this.overlaySprite.height = 32;
+		this.overlaySprite.alpha = 0;
+
 		this.thrustSprites = {};
 
 		this.thrustSprites.forward = new PIXI.Sprite(PIXI.loader.resources["thrust:default:forward"].texture);
@@ -36,7 +44,13 @@ class EntityPlayer extends EntityBase {
 
 		ENT.stageContainer.addChild(this.sprite,
 									this.thrustSprites.forward,
-									this.thrustSprites.backward);
+									this.thrustSprites.backward,
+									this.overlaySprite);
+	}
+
+	onHit() {
+		this.overlaySprite.alpha = 1;
+		this.overlaySprite.tint = 0xFF4444;
 	}
 
 	update() {
@@ -51,6 +65,9 @@ class EntityPlayer extends EntityBase {
 		this.thrustSprites.forward.visible = this.controls.thrustForward;
 		this.thrustSprites.backward.visible = this.controls.thrustBackward;
 
+		this.overlaySprite.alpha = lerp(this.overlaySprite.alpha, 0, 0.05);
+
+		this.sprite.attach(this.overlaySprite);
 		this.sprite.attach(this.thrustSprites.forward);
 		this.sprite.attach(this.thrustSprites.backward);
 	}
