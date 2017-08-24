390325dc567e4f0a6c88ae3bc16df0c82a5fc182
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-24 05:41:40 -0700

    Refactor Base to EntityBase

--- entity/Entity.js
+++ entity/Entity.js
@@ -1,10 +1,10 @@
 module.exports = function(io, ENT, PHYS) {
-	var Base = require("../entity/EntityBase.js")(ENT, PHYS);
+	var EntityBase = require("../entity/EntityBase.js")(ENT, PHYS);
 
 	return {
-		PhysicsDebug: require("../entity/EntityPhysicsDebug.js")(Base, ENT, PHYS),
-		Player: require("../entity/EntityPlayer.js")(Base, ENT, PHYS),
-		Shield: require("../entity/EntityShield.js")(Base, ENT, PHYS),
-		Laser: require("../entity/EntityLaser.js")(Base, ENT, PHYS)
+		PhysicsDebug: require("../entity/EntityPhysicsDebug.js")(EntityBase, ENT, PHYS),
+		Player: require("../entity/EntityPlayer.js")(EntityBase, ENT, PHYS),
+		Shield: require("../entity/EntityShield.js")(EntityBase, ENT, PHYS),
+		Laser: require("../entity/EntityLaser.js")(EntityBase, ENT, PHYS)
 	};
 }
