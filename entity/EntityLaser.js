60e866b859c33d5425d826ad1bed576a062c5b56
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-24 05:49:57 -0700

    Change laser lifespan

--- entity/EntityLaser.js
+++ entity/EntityLaser.js
@@ -3,7 +3,7 @@ module.exports = function(EntityBase, ENT, PHYS) {
 		constructor(data) {
 			super("Laser");
 
-			this.lifespan = 3000;
+			this.lifespan = 1000;
 
 			this.ownerId = data.ownerId;
 			this.thickness = data.thickness || 2;
