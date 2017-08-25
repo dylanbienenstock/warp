0048ded3e334319cefbfb4d07ef71cbbf0cfc7fd
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-24 21:57:08 -0700

    Make EntityLaser create trail effect

--- client/js/entity/EntityLaser.js
+++ client/js/entity/EntityLaser.js
@@ -4,16 +4,27 @@ class EntityLaser extends EntityBase {
 
 		this.lerpFactorPosition = 0.8;
 
+		this.thickness = data.thickness || 2;
+		this.color = data.color || 0xFF0000;
+		this.length = data.length || 64;
+
 		this.graphics = new PIXI.Graphics();
 		this.graphics.x = this.x;
 		this.graphics.y = this.y;
-		this.graphics.lineStyle(data.thickness, data.color, 1);
-		this.graphics.moveTo(-data.length + 8, 0);
-		this.graphics.lineTo(-8, 0);
+		this.graphics.lineStyle(this.thickness, this.color, 1);
+		this.graphics.moveTo(-this.length, 0);
+		this.graphics.lineTo(0, 0);
 		this.graphics.rotation = this.rotation;
 
 		this.sprite = this.graphics;
 		ENT.stageContainer.addChild(this.graphics);
+
+		ENT.createEffect(ENT.newEffect("LaserTrail", {
+			ownerId: this.id,
+			x: this.x,
+			y: this.y
+		}));
+
 	}
 
 	update() {
