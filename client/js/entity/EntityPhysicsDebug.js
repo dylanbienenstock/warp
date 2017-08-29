9952fd83db958e516ed9aabf7a089441eedaad86
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-29 00:01:56 -0700

    Update EntityPhysicsDebug to display sleeping

--- client/js/entity/EntityPhysicsDebug.js
+++ client/js/entity/EntityPhysicsDebug.js
@@ -3,6 +3,7 @@ class EntityPhysicsDebug extends EntityBase {
 		super(data);
 
 		this.info = null;
+		this.sleeping = false;
 
 		this.graphics = new PIXI.Graphics();
 		this.graphics.zIndex = 99999;
@@ -14,42 +15,45 @@ class EntityPhysicsDebug extends EntityBase {
 		super.update();
 
 		this.graphics.visible = ENT.physicsDebug;
-		this.graphics.clear();
-		this.graphics.lineStyle(1, 0x00FF00, 1);
 
-		for (var i = 0; i < this.info.lines.length; i++) {
-			var line = this.info.lines[i];
+		if (this.info != undefined) {
+			this.graphics.clear();
+			this.graphics.lineStyle(1, (this.sleeping ?  0xFF0000 : 0x00FF00), 1);
 
-			this.graphics.moveTo(line.start.x, line.start.y);
-			this.graphics.lineTo(line.end.x, line.end.y);
+			for (var i = 0; i < this.info.lines.length; i++) {
+				var line = this.info.lines[i];
+
+				this.graphics.moveTo(line.start.x, line.start.y);
+				this.graphics.lineTo(line.end.x, line.end.y);
+			}
+
+			for (var i = this.info.circles.length - 1; i >= 0; i--) {
+				var circle = this.info.circles[i];
+
+				this.graphics.drawCircle(circle.position.x, circle.position.y, circle.radius);
+			}
+
+			this.graphics.lineStyle(1, 0xFFFF00, 0.5);
+
+			this.graphics.moveTo(this.info.bounds.minX, this.info.bounds.minY);
+			this.graphics.lineTo(this.info.bounds.maxX, this.info.bounds.minY);
+
+			this.graphics.moveTo(this.info.bounds.minX, this.info.bounds.minY);
+			this.graphics.lineTo(this.info.bounds.minX, this.info.bounds.maxY);
+
+			this.graphics.moveTo(this.info.bounds.maxX, this.info.bounds.minY);
+			this.graphics.lineTo(this.info.bounds.maxX, this.info.bounds.maxY);
+
+			this.graphics.moveTo(this.info.bounds.minX, this.info.bounds.maxY);
+			this.graphics.lineTo(this.info.bounds.maxX, this.info.bounds.maxY);
+
+			this.graphics.lineStyle();
+			this.graphics.beginFill(0xFF0000)
+
+			this.graphics.drawCircle(this.info.origin.x, this.info.origin.y, 1);
+
+			this.graphics.endFill();
 		}
-
-		for (var i = this.info.circles.length - 1; i >= 0; i--) {
-			var circle = this.info.circles[i];
-
-			this.graphics.drawCircle(circle.position.x, circle.position.y, circle.radius);
-		}
-
-		this.graphics.lineStyle(1, 0xFFFF00, 0.5);
-
-		this.graphics.moveTo(this.info.bounds.minX, this.info.bounds.minY);
-		this.graphics.lineTo(this.info.bounds.maxX, this.info.bounds.minY);
-
-		this.graphics.moveTo(this.info.bounds.minX, this.info.bounds.minY);
-		this.graphics.lineTo(this.info.bounds.minX, this.info.bounds.maxY);
-
-		this.graphics.moveTo(this.info.bounds.maxX, this.info.bounds.minY);
-		this.graphics.lineTo(this.info.bounds.maxX, this.info.bounds.maxY);
-
-		this.graphics.moveTo(this.info.bounds.minX, this.info.bounds.maxY);
-		this.graphics.lineTo(this.info.bounds.maxX, this.info.bounds.maxY);
-
-		this.graphics.lineStyle();
-		this.graphics.beginFill(0xFF0000)
-
-		this.graphics.drawCircle(this.info.origin.x, this.info.origin.y, 1);
-
-		this.graphics.endFill();
 	}
 
 	remove() {
