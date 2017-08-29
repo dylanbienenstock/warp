4b41844d3d00dad21ff6be75af885679073b5f78
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-28 22:43:06 -0700

    Add comments and culling

--- client/js/entity/EntityBase.js
+++ client/js/entity/EntityBase.js
@@ -1,3 +1,17 @@
+/*
+
+Special entity variables
+------------------------
+* x, y, rotation: Used to send physics information to client
+* triggers: Used to send trigger events to client
+* lifespan: If set, the entity will be automatically removed after this many milliseconds
+* createdTime: Set automatically if entity has lifespan property
+* networkGlobally: Entities with this property will be networked to all clients regardless of position
+* doNotNetwork: Entities with this property set to true will not be networked
+				Properties of the entity may use this property as well
+
+*/
+
 class EntityBase {
 	constructor(data) {
 		this.id = data.id || -1;
@@ -13,6 +27,18 @@ class EntityBase {
 		if (this.sprite != undefined) {
 			this.sprite.x = lerp(this.sprite.x, this.x, this.lerpFactorPosition || ENT.lerpFactorPosition);
 			this.sprite.y = lerp(this.sprite.y, this.y, this.lerpFactorPosition || ENT.lerpFactorPosition);
+
+			var bounds = this.sprite.getBounds();
+
+			if (bounds.x < ENT.ww && 
+				bounds.x + bounds.width > 0 &&
+				bounds.y < ENT.wh &&
+				bounds.y + bounds.height > 0) {
+
+				this.sprite.visible = true;
+			} else {
+				this.sprite.visible = false;
+			}
 		}
 	}
 
