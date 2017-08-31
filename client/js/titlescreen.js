c8924d11061ac72109c36477abcfdf0678600908
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-08-31 13:13:44 -0700

    Change title screen circle intervals

--- client/js/titlescreen.js
+++ client/js/titlescreen.js
@@ -70,7 +70,7 @@ function updateTitleScreen(baseContainer, titleScreenContainer, gameContainer) {
 var awaitingResponse = false;
 var minCircleAlpha = 0.2;
 var circleIntervals = [
-	4000, 5000, 2000
+	2000, 4000, 8000
 ];
 
 function drawOscillatingCircles(ww, wh) {
