4cca1ae1491ad1069e767c9758f1e41348a3f81c
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-09-03 23:33:29 -0700

    Add heavy laser weapon

---
+++ weapon/WeaponHeavyLaser.js
@@ -0,0 +1,41 @@
+module.exports = function(WeaponBase, ENT, PHYS) {
+	return class WeaponHeavyLaser extends WeaponBase {
+		constructor(player) {
+			super(player);
+
+			this.fireInterval = 445;
+			this.damage = 15;
+			this.shotSpeed = 32;
+		}
+
+		static getListing() {
+			return {
+				displayName: "Heavy Laser",
+				className: "HeavyLaser",
+				price: 0,
+				description: "Fires a single high-powered laser.",
+				stats: {
+					"Type": "Standard laser weapon",
+					"Damage": 15,
+					"Shots/min": 135,
+					"Proj./shot": 1,
+					"Shot speed": 32
+				}
+			};
+		}
+
+		fire(position, angle) {
+			ENT.create(ENT.new("Laser", {
+				ownerId: this.ownerId,
+				damage: this.damage,
+				thickness: 4,
+				color: 0x0055FF,
+				length: 32,
+				x: position.x,
+				y: position.y,
+				angle: angle,
+				speed: this.shotSpeed
+			}));
+		}
+	}
+}
