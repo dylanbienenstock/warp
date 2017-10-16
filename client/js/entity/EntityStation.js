8432df941bb557003a891941520d1de9f3396fe5
Dylan Bienenstock <dylanbienenstock@gmail.com>
2017-10-16 03:49:53 -0700

    Make stations orbit

--- client/js/entity/EntityStation.js
+++ client/js/entity/EntityStation.js
@@ -2,13 +2,17 @@ class EntityStation extends EntityBase {
 	constructor(data) {
 		super(data);
 
-		this.color = data.color || 0x00FF00;
 		this.radius = data.radius || 256;
 		this.alignment = data.alignment || "neutral";
+		this.color = 0xFFFF00;
+
+		if (this.alignment == "good") {
+			this.color = 0x00FF00;
+		} else if (this.alignment == "evil") {
+			this.color = 0xFF0000;
+		}
 
 		this.container = new PIXI.ParticleContainer();
-		this.container.x = this.x;
-		this.container.y = this.y;
 
 		this.outerSprite = new PIXI.Sprite(PIXI.loader.resources["station:outer:" + this.alignment].texture);
 		this.outerSprite.width = this.radius * 2;
@@ -24,6 +28,8 @@ class EntityStation extends EntityBase {
 		this.innerShadowSprite.anchor.set(0.5, 0.5);
 
 		this.sprite = new PIXI.Sprite(PIXI.loader.resources["station:inner:" + this.alignment].texture);
+		this.sprite.x = this.x;
+		this.sprite.y = this.y;
 		this.sprite.width = this.radius * 2;
 		this.sprite.height = this.radius * 2;
 		this.sprite.anchor.set(0.5, 0.5);
@@ -35,15 +41,22 @@ class EntityStation extends EntityBase {
 	update() {
 		super.update();
 
+		addRadarDot(this.sprite.x, this.sprite.y, this.color, 2);
+		addRadarDot(this.sprite.x, this.sprite.y, 0x000000, 4);
+		addRadarDot(this.sprite.x, this.sprite.y, 0x888888, 6);
+
+		if (this.container.visible) {
+			this.outerSprite.position = this.sprite.position;
+			this.innerShadowSprite.position = this.sprite.position;
+		}
+
 		this.outerSprite.rotation += 0.001;
 		this.innerShadowSprite.rotation += 0.00025;
 		this.sprite.rotation += 0.00025;
 	}
 
 	cull(visible) {
-		this.outerSprite.visible = visible;
-		this.innerShadowSprite.visible = visible;
-		this.sprite.visible = visible;
+		this.container.visible = visible;
 	}
 
 	remove() {
--- entity/EntityStation.js
+++ entity/EntityStation.js
@@ -3,10 +3,43 @@ module.exports = function(EntityBase, ENT, PHYS) {
 		constructor(data) {
 			super(data);
 
+			this.networkGlobally = true;
+
+			this.radius = data.radius || 256;
 			this.alignment = data.alignment || "neutral";
-			this.color = data.color || 0x00FF00;
-			this.x = data.x;
-			this.y = data.y;
+			this.orbitEntityId = data.orbitEntityId;
+			this.orbitRadius = data.orbitRadius;
+			this.orbitOffset = data.orbitOffset;
+			this.orbitSpeedDivisor = data.orbitSpeedDivisor;
+		}
+
+		update() {
+			super.update();
+
+			this.orbit();
+		}
+
+		orbit() {
+			if (this.orbitEntityId != undefined) {
+				ENT.getById(this.orbitEntityId, function(orbitEntity) {
+					if (orbitEntity.physicsObject != undefined) {
+						var now = Date.now();
+
+						var orbitX = Math.cos(now / this.orbitSpeedDivisor + this.orbitOffset) * (this.orbitRadius);
+						var orbitY = Math.sin(now / this.orbitSpeedDivisor + this.orbitOffset) * (this.orbitRadius);
+
+						this.x = orbitEntity.physicsObject.x + orbitX;
+						this.y = orbitEntity.physicsObject.y + orbitY;
+					}
+				}.bind(this));
+			}	
+		}
+
+		network() {
+			ENT.sendProperties(this, {
+				x: this.x,
+				y: this.y
+			});
 		}
 	}
 }
